179be72dca7121f8bbd5bbf382c0037e82abf44e
Munir Arabi <munirarabi04@gmail.com>
2022-07-07 01:11:34 -0300

    adicionando promisses, try catch, async e await

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,103 +1,80 @@
-const mysql = require("../mysql").pool;
+const mysql = require("../mysql");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-exports.postUserRegister = (req, res, next) => {
-  mysql.getConnection((error, conn) => {
-    if (error) {
-      return res.status(500).send({ error: error });
+exports.postUserRegister = async (req, res, next) => {
+  try {
+    const insertQuery = `SELECT id_user, email, password FROM users WHERE email = ?;`;
+    const result = await mysql.execute(insertQuery, [req.body.email]);
+
+    if (result.length > 0) {
+      res.status(409).send({
+        message: "Usuário já cadastrado",
+      });
+    } else {
+      bcrypt.hash(req.body.password, 10, async (errBcrypt, hash) => {
+        if (errBcrypt) {
+          return res.status(500).send({ error: errBcrypt });
+        }
+
+        const insertQuery2 = `INSERT INTO users (email, password) VALUES (?,?);`;
+        const result2 = await mysql.execute(insertQuery2, [
+          req.body.email,
+          hash,
+        ]);
+
+        const response = {
+          message: "Usuário criado com sucesso",
+          userCreated: {
+            id_user: result2.insertId,
+            email: req.body.email,
+          },
+        };
+
+        return res.status(201).send(response);
+      });
     }
-
-    conn.query(
-      `SELECT id_user, email, password FROM users WHERE email = ?`,
-      [req.body.email],
-      (error, result) => {
-        if (error) {
-          return res.status(500).send({ error: error });
-        }
-
-        if (result.length > 0) {
-          res.status(409).send({
-            message: "Usuário já cadastrado",
-          });
-        } else {
-          bcrypt.hash(req.body.password, 10, (errBcrypt, hash) => {
-            if (errBcrypt) {
-              return res.status(500).send({ error: errBcrypt });
-            }
-
-            conn.query(
-              `INSERT INTO users (email, password) VALUES (?,?)`,
-              [req.body.email, hash],
-              (error, result) => {
-                conn.release();
-                if (error) {
-                  return res.status(500).send({ error: error });
-                }
-
-                const response = {
-                  message: "Usuário criado com sucesso",
-                  userCreated: {
-                    id_user: result.insertId,
-                    email: req.body.email,
-                  },
-                };
-
-                return res.status(201).send(response);
-              }
-            );
-          });
-        }
-      }
-    );
-  });
+  } catch (error) {
+    return res.status(500).send({ error: error });
+  }
 };
 
-exports.postUserLogin = (req, res, next) => {
-  mysql.getConnection((error, conn) => {
-    if (error) {
-      return res.status(500).send({ error: error });
+exports.postUserLogin = async (req, res) => {
+  try {
+    const insertQuery = `SELECT id_user, email, password FROM users WHERE email = ?;`;
+    const result = await mysql.execute(insertQuery, [req.body.email]);
+
+    if (result.length < 1) {
+      return res.status(401).send({ message: "Falha na autenticação" });
     }
 
-    const insertQuery = `SELECT id_user, email, password FROM users WHERE email = ?`;
-    conn.query(insertQuery, [req.body.email], (error, result, fields) => {
-      conn.release();
+    const idUser = result[0].id_user;
+    const emailUser = result[0].email;
+
+    bcrypt.compare(req.body.password, result[0].password, (error, result) => {
       if (error) {
-        return res.status(500).send({ error: error, type: "Erro de query" });
-      }
-
-      if (result.length < 1) {
         return res.status(401).send({ message: "Falha na autenticação" });
       }
 
-      const idUser = result[0].id_user;
-      const emailUser = result[0].email;
+      if (result) {
+        const token = jwt.sign(
+          {
+            id_user: idUser,
+            email: emailUser,
+          },
+          process.env.JWT_KEY,
+          {
+            expiresIn: "1h",
+          }
+        );
 
-      bcrypt.compare(req.body.password, result[0].password, (error, result) => {
-        if (error) {
-          return res.status(401).send({ message: "Falha na autenticação" });
-        }
-
-        if (result) {
-          const token = jwt.sign(
-            {
-              id_user: idUser,
-              email: emailUser,
-            },
-            process.env.JWT_KEY,
-            {
-              expiresIn: "1h",
-            }
-          );
-
-          return res.status(200).send({
-            message: "Autenticado com sucesso",
-            token: token,
-          });
-        }
-
-        return res.status(401).send({ message: "Falha na autenticação" });
-      });
+        return res.status(200).send({
+          message: "Autenticado com sucesso",
+          token: token,
+        });
+      }
     });
-  });
+  } catch (error) {
+    return res.status(500).send({ error: error });
+  }
 };

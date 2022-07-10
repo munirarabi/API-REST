850fd2f99669f75a2d7af7d8c2d06d783f7e58cf
Munir Arabi <munirarabi04@gmail.com>
2022-07-10 00:35:22 -0300

    alterando permissoes do headers

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use(bodyParser.json());
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
   res.header(
     "Access-Control-Allow-Header",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
